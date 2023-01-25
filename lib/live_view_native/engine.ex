4edf4acd0bb5e08fc423a4a77a23145f433cd15e
Carson Katri <Carson.katri@gmail.com>
2023-01-25 14:27:15 -0500

    Add handle_token implementations for namespaced slots

--- lib/live_view_native/engine.ex
+++ lib/live_view_native/engine.ex
@@ -461,6 +461,26 @@ defmodule LiveViewNative.Engine do
     end
   end
 
+  # Slot (for HTML element)
+
+  defp handle_token(
+    {:tag_open, ":" <> slot_name, _attrs, tag_meta} = token,
+    %{tags: [{:tag_open, <<first, _::binary>> = parent_name, _, _} | _]} = state
+  ) when not (first in ?A..?Z) and first != ?. do
+    # Translate into namespaced element `parent:slot_name`
+    token = put_elem(token, 1, parent_name <> elem(token, 1))
+    handle_token(token, state)
+  end
+
+  defp handle_token(
+    {:tag_close, ":" <> slot_name = tag_name, _tag_close_meta} = token,
+    %{tags: [{:tag_open, open_slot_name, _, _}, {:tag_open, <<first, _::binary>> = parent_name, _, _} | _]} = state
+  ) when not (first in ?A..?Z) and first != ?. do
+      # Translate into namespaced element `parent:slot_name`
+    token = put_elem(token, 1, parent_name <> elem(token, 1))
+    handle_token(token, state)
+  end
+
   # Slot
 
   defp handle_token({:tag_open, ":inner_block", _attrs, meta}, state) do
