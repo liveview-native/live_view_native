0a9d48adaaa2dd8e66174a1ccc749d31782583d7
Carson Katri <Carson.katri@gmail.com>
2023-03-16 11:56:54 -0400

    Allow capitalized tags

--- lib/live_view_native/tag_engine.ex
+++ lib/live_view_native/tag_engine.ex
@@ -4,8 +4,13 @@ defmodule LiveViewNative.TagEngine do
   def classify_type(":inner_block"), do: {:error, "the slot name :inner_block is reserved"}
   def classify_type(":" <> name), do: {:slot, String.to_atom(name)}
 
-  def classify_type(<<first, _::binary>> = name) when first in ?A..?Z,
-    do: {:remote_component, String.to_atom(name)}
+  def classify_type(<<first, _::binary>> = name) when first in ?A..?Z do
+    if String.contains?(name, ".") do
+      {:remote_component, String.to_atom(name)}
+    else
+      {:tag, name}
+    end
+  end
 
   def classify_type("." <> name),
     do: {:local_component, String.to_atom(name)}
