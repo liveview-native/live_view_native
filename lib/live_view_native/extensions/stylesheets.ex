5b24ac157058a9751eef375bc2e611c3ce33f0c9
May Matyi <may@matyi.net>
2023-10-12 21:05:15 -0700

    Add stylesheet extension

---
+++ lib/live_view_native/extensions/stylesheets.ex
@@ -0,0 +1,26 @@
+defmodule LiveViewNative.Extensions.Stylesheets do
+  @moduledoc false
+
+  @doc """
+  This macro adds support for LiveView Native's stylesheet DSL which
+  enables configuration of visual properties for native platforms
+  (i.e. SwiftUI modifiers, Jetpack Compose themes, etc.)
+  """
+  defmacro __using__(opts \\ []) do
+    module = opts[:module]
+    stylesheet = opts[:stylesheet]
+
+    quote bind_quoted: [module: module, stylesheet: stylesheet] do
+      def __compiled_stylesheet__ do
+        class_tree_module = Module.safe_concat([LiveViewNative, Internal, ClassTree, unquote(module)])
+        class_tree = apply(class_tree_module, :class_tree, [])
+        class_names =
+          class_tree
+          |> Map.values()
+          |> List.flatten()
+
+        apply(unquote(stylesheet), :compile_string, [class_names])
+      end
+    end
+  end
+end
