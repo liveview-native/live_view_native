a571828459e4bc919c2341149eec6cf74cddfc5a
BrooklinJazz <brooklinjazzmyers@gmail.com>
2024-01-17 13:10:56 -0500

    switch to latest kino_live_view_native version

--- mix.exs
+++ mix.exs
@@ -92,8 +92,8 @@ defmodule LiveViewNative.MixProject do
       "guides/ex_doc_notebooks/create-a-swiftui-application.md",
       "guides/ex_doc_notebooks/common-swiftui-views.md",
       "guides/ex_doc_notebooks/stylesheets.md",
-      "guides/ex_doc_notebooks/interactive-swiftui-views.md"
-      # "guides/ex_doc_notebooks/navigation.md"
+      "guides/ex_doc_notebooks/interactive-swiftui-views.md",
+      "guides/ex_doc_notebooks/navigation.md"
       # "guides/ex_doc_notebooks/forms-and-validation.md"
       # "guides/ex_doc_notebooks/deployment.md"
     ]
