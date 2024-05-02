88b2baa66eda81d14b58f50213450d566d3b924f
Brian Cardarella <bcardarella@gmail.com>
2024-05-02 00:03:59 -0400

    Fix interface fetcher

--- lib/live_view_native/utils.ex
+++ lib/live_view_native/utils.ex
@@ -2,13 +2,21 @@ defmodule LiveViewNative.Utils do
   @moduledoc false
   alias Phoenix.LiveView.Socket
 
-  def get_format(%Socket{private: %{connect_info: %{private: %{phoenix_format: format}}}}), do: format
-  def get_format(%Socket{private: %{connect_params: %{"_format" => format}}}), do: format
-  def get_format(_socket), do: "html"
+  def get_format(%Socket{private: %{connect_info: %{private: %{phoenix_format: format}}}}),
+    do: format
+  def get_format(%Socket{private: %{connect_params: %{"_format" => format}}}),
+    do: format
+  def get_format(_socket),
+    do: "html"
 
-  def get_interface(%{socket: %Socket{private: %{connect_info: %{params: %{"_interface" => interface}}}}}), do: interface
-  def get_interface(%{conn: %Plug.Conn{params: %{"_interface" => interface}}}), do: interface
-  def get_interface(_socket), do: %{}
+  def get_interface(%{socket: %Socket{private: %{connect_params: %{"_interface" => interface}}}}),
+    do: interface
+  def get_interface(%{conn: %Plug.Conn{params: %{"_interface" => interface}}}),
+    do: interface
+  def get_interface(%{socket: %Socket{private: %{connect_info: %Plug.Conn{params: %{"_interface" => interface}}}}}),
+    do: interface
+  def get_interface(_assigns),
+    do: %{}
 
   def stringify_format(format) when is_binary(format), do: format
   def stringify_format(format) when is_atom(format), do: Atom.to_string(format)
