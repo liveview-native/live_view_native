f6d864e432d9ab0dc25a179acbae64229fe05515
Brian Cardarella <bcardarella@gmail.com>
2024-10-02 17:05:43 -0400

    0.3.1 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule LiveViewNative.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @source_url "https://github.com/liveview-native/live_view_native"
 
   def project do
