5d0972bc5940384abf9883ab956436ff9e16aa89
Brian Cardarella <bcardarella@gmail.com>
2024-12-14 19:08:03 -0500

    Restore dep

--- mix.exs
+++ mix.exs
@@ -32,8 +32,7 @@ defmodule LiveViewNative.MixProject do
     [
       {:phoenix, "~> 1.7.0"},
       {:phoenix_view, "~> 2.0"},
-      # {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, path: "../phoenix_live_view", override: true},
+      {:phoenix_live_view, "~> 1.0.0"},
       {:phoenix_live_reload, "~> 1.4", only: :test},
       {:phoenix_template, "~> 1.0.4"},
       {:phoenix_html, "~> 3.3 or ~> 4.0 or ~> 4.1"},
